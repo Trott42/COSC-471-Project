27615126ff4513f4f0b3aa2da850c4cda8a9dc48
Huda Al-Regib <73912953+hudz1234@users.noreply.github.com>
2023-10-31 19:53:22 -0400

    homepage works you can now tweet

--- src/app/comp/home/home.component.ts
+++ src/app/comp/home/home.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  tweets: any[] = [
+    { user: 'User 1', text: 'This is a sample tweet.' },
+    { user: 'User 2', text: 'Another sample tweet here.' }
+  ];
+  newTweetText: string = '';
 
+  postTweet() {
+    if (this.newTweetText.trim() !== '') {
+      this.tweets.push({ user: 'User', text: this.newTweetText });
+      this.newTweetText = '';
+    }
+  }
 }
